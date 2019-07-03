784c6079cfe5ed0a07e4efa9207b60d912cbf69f
Dinkie <david.churchill72@gmail.com>
2019-07-03 14:15:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ function addEvent(day, desc, start, end){
   }
   maxID += 1;
   save();
-  if(mon.length + tue.length + wed.length + thu.length + fri.length + sat.length + sun.length >= 5){
+  if(mon.length + tue.length + wed.length + thu.length + fri.length + sat.length + sun.length == 5){
     window.top.location = "http://localhost:8080/link/CompleteScheduleMaker/";
   }
 }
