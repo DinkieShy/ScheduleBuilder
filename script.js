168114a1efbc6ea0c03aa58ae71784cf1c902e28
Dinkie <david.churchill72@gmail.com>
2019-07-19 09:23:36 +0100

    Update deeplinks

--- script.js
+++ script.js
@@ -89,7 +89,7 @@ function addEvent(day, desc, start, end){
   maxID += 1;
   save();
   if(mon.length + tue.length + wed.length + thu.length + fri.length + sat.length + sun.length == 5 && completed == "false"){
-    window.top.location = "http://localhost:8080/link/CompleteScheduleMaker/";
+    window.top.location = "https://countdowntolincoln100-sandbox.mxapps.io/link/CompleteScheduleMaker/";
   }
 }
 
